4443ab23df315895372330a48642b3f49e37807e
Jeromy Fu <fuji246@gmail.com>
2023-03-03 18:03:50 -0800

    fix run script

--- sender/cellsim-runner.sh
+++ sender/cellsim-runner.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-nohup /home/ubuntu/multisend/sender/cellsim /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 0 LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr &
+#nohup /home/ubuntu/multisend/sender/cellsim /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 0 LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr &
+#nohup sudo ./cellsim trace-analysis/uplink-1677798915.pps trace-analysis/downlink-1677798915.pps 0.0 eth0 eth1 > /tmp/cellsim-stdout 2>/tmp/cellsim-stderr &
+nohup sudo ./cellsim 200.pps 1000.pps 0.0 eth0 eth1 > /tmp/cellsim-stdout 2>/tmp/cellsim-stderr &
