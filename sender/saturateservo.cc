a469e0bc32b4813a0d2b9de3c33a6fcb71235729
Jeromy Fu <fuji246@gmail.com>
2023-03-06 22:50:18 -0800

    large packet may not get through

--- sender/saturateservo.cc
+++ sender/saturateservo.cc
@@ -119,7 +119,7 @@ void SaturateServo::tick( void )
       outgoing.recv_timestamp = 0;
       outgoing.sender_id = _send_id;
 
-      _send.send( Socket::Packet( _remote, outgoing.str( 1400 ) ) );
+      _send.send( Socket::Packet( _remote, outgoing.str( 1300 ) ) );
 
       /*
       printf( "%s pid=%d DATA SENT %d senderid=%d seq=%d, send_time=%ld, recv_time=%ld\n",
@@ -139,7 +139,7 @@ void SaturateServo::tick( void )
     outgoing.recv_timestamp = 0;
     outgoing.sender_id = _send_id;
 
-    _send.send( Socket::Packet( _remote, outgoing.str( 1400 ) ) );
+    _send.send( Socket::Packet( _remote, outgoing.str( 1300 ) ) );
 
     /*
     printf( "%s pid=%d DATA SENT senderid=%d seq=%d, send_time=%ld, recv_time=%ld\n",
