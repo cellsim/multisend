a6f057d79803686fec8bb937e2e16921e5e7dcc5
Jeromy Fu <fuji246@gmail.com>
2023-05-09 16:45:19 -0700

    fix compile issue in arm32

--- sender/acker.cc
+++ sender/acker.cc
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <inttypes.h>
 
 #include "acker.hh"
 #include "payload.hh"
@@ -50,7 +51,7 @@ void Acker::recv( void )
   outgoing.ack_number = contents->sequence_number;
   _send.send( Socket::Packet( _remote, outgoing.str( sizeof( SatPayload ) ) ) );
 
-   fprintf( _log_file,"%s DATA RECEIVED senderid=%d, seq=%d, send_time=%ld, recv_time=%ld, 1delay=%.4f \n",
+   fprintf( _log_file,"%s DATA RECEIVED senderid=%d, seq=%d, send_time=%" PRIu64 ", recv_time=%" PRIu64 ", 1delay=%.4f \n",
       _name.c_str(),  _server ? contents->sender_id : _ack_id, contents->sequence_number, contents->sent_timestamp, contents->recv_timestamp,oneway ); 
 }
 
